f5af08579bda5a894ed4e47b59d4b0000593c73f
RamunnoAJ <ramunnoagustin@gmail.com>
2023-03-01 13:16:27 -0300

    agrega: dependencia de cypress y prueba básica

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    }
+  },
+  viewportHeight: 900,
+  viewportWidth: 1280
+})
